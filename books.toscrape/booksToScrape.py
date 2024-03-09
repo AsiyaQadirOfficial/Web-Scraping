e9de3c7597506786ee7b2bfa233aa5dc86382553
AsiyaQadir <asiyaqadirofficial@gmail.com>
2024-03-09 18:07:34 +0500

    getting the exact link of your searched book

--- books.toscrape/booksToScrape.py
+++ books.toscrape/booksToScrape.py
@@ -2,17 +2,14 @@
 import requests
 from bs4 import BeautifulSoup
 import os
+import numpy as np  
 
 url = 'https://books.toscrape.com/'
-
 html_content = requests.get(url).text
 soup = BeautifulSoup(html_content, 'html.parser')
 books_list = soup.find('ul', class_='nav nav-list')
 book_list = books_list.text.replace(' ','')
 books = book_list.split()
-
-# for book in books:
-#     print(book)
 
 anchor_tags = books_list.find_all('a', href=True)
 flag = False
@@ -21,13 +18,12 @@ with open('allBooks.txt','w') as f:
         href = url + anchor_tag.get('href')
         parts = href.split('/')
         specific_part = parts[-2]
-        # book_name = specific_part[0:-3]
         book_name = specific_part.split('_')[0]
         print(book_name)
         f.write(f'Book Type: {book_name} \n')
-        f.write(f'Link: {href} \n\n')
+        f.write(f'{href} \n\n')
 
-book_type = input('Enter your Book type here: ')
+book_type = input('Enter your Book category here: ')
 found = False  
 for book in books:
     mybook = book.lower()
@@ -47,8 +43,25 @@ if found:
                 print(searched_book)
                 for result in searched_book:
                     with open('relatedBooks.txt', 'a+') as f:
-                        f.write(f'{result}')   
-
+                        f.write(f'{result}') 
 else:
     print('Not Found!')
 
+
+with open('relatedBooks.txt','r') as f:
+    content = f.readlines()  
+    # print(content)
+
+    arr = np.array([content])
+    # print(arr)
+    
+    # x = np.where(arr == book_type)
+    i = [i for i, line in enumerate(content) if book_type in line.split()]    #for index
+    x = [line for line in content if book_type in line.split()]             #for content
+    # print(i)
+    # print(x)
+    link_index = i[0] + 1
+    searched_link = content[link_index]
+    # print(searched_link)
+
+    
