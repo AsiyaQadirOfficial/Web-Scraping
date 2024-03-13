17e4e1a39e64a6f97bac1f4c239a6ff7baac22d2
AsiyaQadir <asiyaqadirofficial@gmail.com>
2024-03-13 15:52:56 +0500

    scraping fine but not extracting data of books having more than 1 pages yet

--- books.toscrape/booksToScrape.py
+++ books.toscrape/booksToScrape.py
@@ -20,6 +20,7 @@ with open('allBooks.txt','w') as f:
         specific_part = parts[-2]
         book_name = specific_part.split('_')[0]
         print(book_name)
+
         f.write(f'Book Type: {book_name} \n')
         f.write(f'{href} \n\n')
 
@@ -38,10 +39,10 @@ if found:
         os.remove('relatedBooks.txt')
     with open('allBooks.txt', 'r') as content:
         all_books = content.readlines()
-        for searched_book in all_books:
-            if book_type in searched_book:
-                print(searched_book)
-                for result in searched_book:
+        for related_book in all_books:
+            if book_type in related_book:
+                # print(related_book)
+                for result in related_book:
                     with open('relatedBooks.txt', 'a+') as f:
                         f.write(f'{result}') 
 else:
@@ -50,18 +51,80 @@ else:
 
 with open('relatedBooks.txt','r') as f:
     content = f.readlines()  
-    # print(content)
-
     arr = np.array([content])
-    # print(arr)
     
     # x = np.where(arr == book_type)
-    i = [i for i, line in enumerate(content) if book_type in line.split()]    #for index
-    x = [line for line in content if book_type in line.split()]             #for content
-    # print(i)
-    # print(x)
+    i = [i for i, line in enumerate(content) if book_type.lower() in line.split()]    #matched booktype line's index
+    x = [line for line in content if book_type.lower() in line.split()]     #matched booktype content
     link_index = i[0] + 1
-    searched_link = content[link_index]
-    # print(searched_link)
+    scraped_link = content[link_index]
+    split_link = scraped_link.split('/')[0:-1]
+    required_link = ('/').join(split_link)
+
+    url_response = requests.get(required_link)
+    htmlContent = url_response.text
+    soup2 = BeautifulSoup(htmlContent, 'html.parser')
+    # soup2 = BeautifulSoup(htmlContent, 'html.parser').prettify()
+    # print(soup2)
+
+    alias = soup2.find_all('li', class_ = 'col-xs-6 col-sm-4 col-md-3 col-lg-3')
+    if os.stat('searchedBooks.txt').st_size != 0:
+        os.remove('searchedBooks.txt')
+    for li in alias:
+        # Title of book and link here:
+        book_title = li.h3.string
+        print('Book Title:',book_title)
+
+        searched_book_scraped_link = li.h3.a.get('href')
+        searched_book_split_link = searched_book_scraped_link.split('/')[3:]
+        searched_book_join_link2 = ('/').join(searched_book_split_link)
+        
+        searched_book_main_link = scraped_link.split('/')[0:-4]
+        searched_book_join_link1 = ('/').join(searched_book_main_link)
+
+        searched_book_link = searched_book_join_link1 + '/' + searched_book_join_link2
+        print('Book Link:',searched_book_link)
+
+
+
+        # Source Image Link here:
+        img_tags = li.find('img', class_ = 'thumbnail') 
+        source = img_tags.get('src')
+        image_source = source.split('/')[4:]
+        title_image_source = ('/').join(image_source)
+        title_image_source_link = url + title_image_source
+        print('Title Image - Source:',title_image_source_link)
+
+
+        
+        
+        # book price and stock availability here:
+        product_details = li.find('div', class_='product_price')
+        
+        product_price = product_details.find('p',class_='price_color')
+        book_price = product_price.string
+        print('Book Price:',book_price)
+
+        product_availability = product_details.find('p',class_='instock availability')
+        stock_availability = product_availability.text.strip()
+        print('Stock Availability:',stock_availability)
+
+
+
+        # rating stars
+        rate_class = li.p['class']
+        star_rate = rate_class[1]
+        # print('Rate Class:',rate_class)
+        print('Star Rate:',star_rate)
+        print()
+
+
+        with open('searchedBooks.txt', 'a') as file:
+            file.write(f'Book Title: {book_title}\n')
+            file.write(f'Book Link: {searched_book_link}\n')
+            file.write(f'Title Image Source: {title_image_source_link}\n')
+            file.write(f'Book Price: {book_price}\n')
+            file.write(f'Star Rate:: {star_rate}\n')
+            file.write(f'Stock Availability: {stock_availability}\n\n')
 
     
